7319578b196fbf5fc4446c120559479ac1104c46
jestopjose <jestopjose99@gmail.com>
2026-01-18 00:13:43 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,13 @@
-import { defineConfig } from "astro/config";
-import netlify from "@astrojs/netlify";
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
 export default defineConfig({
-  output: "server",
-  adapter: netlify(),
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
+  server: {
+    port: Number(process.env.PORT) || 4321,
+    host: true
+  }
 });
