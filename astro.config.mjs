da6f17fb5888c744aea0a47b5377869386d48fa3
jestopjose <jestopjose99@gmail.com>
2026-01-17 23:33:55 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  output: 'server',
-  adapter: netlify({
-    edgeMiddleware: false,
-  }),
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
