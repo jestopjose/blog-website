1b828ddf4216cda2a387d9387947c2492f8611f6
jestopjose <jestopjose99@gmail.com>
2026-01-17 23:09:27 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/server';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
